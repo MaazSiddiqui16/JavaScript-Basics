127efa3ad7a0eb74d3cd6df1a1acf7a8f8231306
MA Siddiqui <siddiquimaaz94@gmail.com>
2026-06-22 20:45:44 +0500

    Folder renamed

--- JavaScript_Objects/4_deleteOperator.js
+++ JavaScript_Objects/4_deleteOperator.js
@@ -7,7 +7,7 @@ let human = {
 console.log("\nBefore Deleting the property of 'human' Object." );
 console.log(human);
 
-delete human.tail; //using 'delete' keyword to delete the property of an object.
+console.log(delete human.tail); //using 'delete' keyword to delete the property of an object.
 
 console.log("\nAfter Deleting the property of 'human' Object." );
 console.log(human);
